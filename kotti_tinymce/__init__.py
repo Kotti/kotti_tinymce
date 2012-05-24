e5fb85bc92070f49b1e6d429895d346157b917b7
Daniel Nouri <daniel.nouri@gmail.com>
2012-05-24 13:53:49 +0200

    Add more resources from collective/tinymce

--- kotti_tinymce/__init__.py
+++ kotti_tinymce/__init__.py
@@ -3,6 +3,8 @@ from pyramid.renderers import render
 from kotti.views.slots import register
 from kotti.views.slots import RenderEditInHead
 
+TINYMCE_SRC = 'kotti_tinymce:Products.TinyMCE/Products/TinyMCE/skins/tinymce/'
+
 def render_resource_links(context, request):
     return render('kotti_tinymce:templates/resources.pt', {}, request)
 
@@ -15,7 +17,22 @@ def kotti_configure(settings):
 def includeme(config):
     config.add_static_view(
         name='static-kotti-tinymce-skins',
-        path='kotti_tinymce:Products.TinyMCE/Products/TinyMCE/skins/tinymce/',
+        path=TINYMCE_SRC,
+        )
+
+    config.override_asset(
+        to_override=TINYMCE_SRC + 'themes/advanced/skins/plone/ui.css',
+        override_with='kotti_tinymce:static/ui.css',
+        )
+
+    config.override_asset(
+        to_override=TINYMCE_SRC + 'themes/advanced/skins/plone/content.css',
+        override_with='kotti_tinymce:static/content.css',
+        )
+
+    config.override_asset(
+        to_override=TINYMCE_SRC + 'plugins/inlinepopups/skins/plonepopup/window.css',
+        override_with='kotti_tinymce:static/window.css',
         )
 
     register(RenderEditInHead, None, render_resource_links)
