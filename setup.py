346454534d2ce47011301ef2f9dd14b8f834a7b9
Andreas kaiser <disko@binary-punks.com>
2012-08-06 13:18:03 +0200

    Update changelist, version and manifest.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 setup(name='kotti_tinymce',
-      version='0.2.3',
+      version='0.2.4.dev',
       description="TinyMCE plugins for Kotti",
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
