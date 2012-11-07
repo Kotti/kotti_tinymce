b46e76d73cd5a581d039c8cd9a6707f33df9fe47
Andreas kaiser <disko@binary-punks.com>
2012-11-07 09:25:09 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 setup(name='kotti_tinymce',
-      version='0.3.1dev',
+      version='0.3.1',
       description="TinyMCE plugins for Kotti",
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
