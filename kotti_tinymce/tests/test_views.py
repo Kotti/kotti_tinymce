596deff5d2b2a6e590cedd3212b69072bc9b650c
Nuno Teixeira <nteixeira@consipere.com>
2012-11-28 12:15:49 +0000

    Check uploaded instances type

--- kotti_tinymce/tests/test_views.py
+++ kotti_tinymce/tests/test_views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from kotti.resources import get_root
 from kotti.resources import Document
+from kotti.resources import File
 from kotti.resources import Image
 
 from kotti_tinymce import KottiTinyMCE
@@ -97,18 +98,20 @@ def test_upload(db_session, dummy_request):
     assert dummy_request.session.pop_flash('error') == \
         [u'Please select a file to upload.']
 
-    dummy_file.filename = 'file.txt'
-    dummy_file.type = 'text/plain'
+    dummy_file.filename = u'file.txt'
+    dummy_file.type = u'text/plain'
     res = kt.upload()
     assert res.status == '302 Found'
     assert res.location == 'http://example.com/title/@@kottibrowser'
     assert dummy_request.session.pop_flash('success') == \
         [u'Successfully uploaded.']
+    assert isinstance(root.get(u'title'), File)
 
-    dummy_file.filename = 'file.jpg'
-    dummy_file.type = 'image/jpeg'
+    dummy_file.filename = u'file.jpg'
+    dummy_file.type = u'image/jpeg'
     res = kt.upload()
     assert res.status == '302 Found'
     assert res.location == 'http://example.com/title-1/@@kottibrowser'
     assert dummy_request.session.pop_flash('success') == \
         [u'Successfully uploaded.']
+    assert isinstance(root.get(u'title-1'), Image)
