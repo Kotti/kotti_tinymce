b450d5a0338222fbb5fa36e780de5d7b528545fd
Andreas Kaiser <disko@binary-punks.com>
2014-11-29 06:41:21 +0100

    Remove dependency from js.tinymce, we're bundling it ourselves now.  Copy additional plugins to the plugins folder.

--- gulpfile.js
+++ gulpfile.js
@@ -18,17 +18,32 @@ gulp.task('copy-vendor', function () {
 
 gulp.task('uglify', function() {
   gulp.src([dest + '/*.js',
-            '!' + dest + '/*.min.js'],
+            dest + '/codemirror/*js',
+            '!' + dest + '**/*.min.js'],
            {base: dest})
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(dest));
 });
 
+gulp.task('copy-plugins', function () {
+  gulp.src(dest + '/kottiimage_plugin.js',
+           {base: dest})
+    .pipe(rename('plugin.js'))
+    .pipe(gulp.dest(dest + '/plugins/kottiimage'));
+  gulp.src(dest + '/kottiimage_plugin.min.js',
+           {base: dest})
+    .pipe(rename('plugin.min.js'))
+    .pipe(gulp.dest(dest + '/plugins/kottiimage'));
+  gulp.src(dest + '/codemirror/**/*.*',
+           {base: dest})
+    .pipe(gulp.dest(dest + '/plugins'));
+});
 // Default task
 gulp.task(
   'default', [
     'copy-vendor',
-    'uglify'
+    'uglify',
+    'copy-plugins'
   ]
 );
